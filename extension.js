359e9c47dfda124c478627c0a630e48621379ca0
Joris Warmbier <siroj42@t-online.de>
2020-08-27 22:37:56 +0200

    Finish up panel and dash support

--- extension.js
+++ extension.js
@@ -1,18 +1,19 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const workspaceBox = imports.ui.workspaceThumbnail.ThumbnailsBox;
 
 function enable() {
     Main.panel.actor.remove_style_class_name('solid');
     Main.panel.actor.add_style_class_name('shell-transparency');
 
-    Main.overview.dash.actor.remove_style_class_name('solid');
-    Main.overview.dash.actor.add_style_class_name('shell-transparency');
+    Main.overview.dash._container.remove_style_class_name('solid');
+    Main.overview.dash._container.add_style_class_name('shell-transparency');
 }
 
 function disable() {
     Main.panel.actor.remove_style_class_name('shell-transparency');
     Main.panel.actor.add_style_class_name('solid');
 
-    Main.overview.dash.actor.remove_style_class_name('shell-transparency');
-    Main.overview.dash.actor.add_style_class_name('solid');
+    Main.overview.dash._container.remove_style_class_name('shell-transparency');
+    Main.overview.dash._container.add_style_class_name('solid');
 }
