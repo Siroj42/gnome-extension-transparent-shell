85e5150db0ca4d72ba088d8d56d005179108cfef
Joris Warmbier <siroj42@t-online.de>
2020-09-09 20:17:49 +0200

    Make compatible with the Multi Monitors Add-on Extension The Workspace switcher and the panel of the Multi Monitors Add-On extension are now also affected by the transparency. Also, some code clean-up.

--- extension.js
+++ extension.js
@@ -8,8 +8,16 @@ function enable() {
     Main.panel._rightCorner.add_style_class_name('shell-transparency');
 
     Main.overview.dash._container.add_style_class_name('shell-transparency');
-    
-    Main.overview._overview._controls._thumbnailsBox.add_style_class_name('shell-transparency');
+
+    try {
+        Main.mmPanel.forEach(function (mmpanel) {
+	    mmpanel.add_style_class_name('shell-transparency');
+	    mmpanel._leftCorner.add_style_class_name('shell-transparency');
+	    mmpanel._rightCorner.add_style_class_name('shell-transparency');
+	});
+    } catch (error) {
+        return;
+    }
 }
 
 function disable() {
@@ -17,7 +25,17 @@ function disable() {
     Main.panel._leftCorner.remove_style_class_name('shell-transparency');
     Main.panel._rightCorner.remove_style_class_name('shell-transparency');
 
+    Main.mmPanel.forEach(mmpanel => mmpanel.add_style_class_name('shell-transparency'));
+
     Main.overview.dash._container.remove_style_class_name('shell-transparency');
     
-    Main.overview._overview._controls._thumbnailsBox.remove_style_class_name('shell-transparency');
+    try {
+        Main.mmPanel.forEach(function (mmpanel) {
+            mmpanel.remove_style_class_name('shell-transparency');
+            mmpanel._leftCorner.remove_style_class_name('shell-transparency');
+            mmpanel._rightCorner.remove_style_class_name('shell-transparency');
+        });
+    } catch (error) {
+        return;
+    }
 }
