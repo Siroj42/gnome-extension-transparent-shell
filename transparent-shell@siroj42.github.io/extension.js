4985619a5c0dcf91ae0dca3bb82bc3b20c12b353
james <86951780+reversesigh@users.noreply.github.com>
2023-11-16 13:41:06 -0500

    Update extension.js

--- transparent-shell@siroj42.github.io/extension.js
+++ transparent-shell@siroj42.github.io/extension.js
@@ -1,32 +1,33 @@
-const Main = imports.ui.main;
-const Gio = imports.gi.Gio;
-const ExtensionUtils = imports.misc.extensionUtils;
+// extension.js
+import { log } from 'utils';
+import { main, overview, gi, misc } from 'imports.ui';
+import { ExtensionUtils } from 'imports.misc.extensionUtils';
 
 const Extension = ExtensionUtils.getCurrentExtension();
 
-const settingskeys = {
+export const settingskeys = {
   "top-panel": 0,
   dash: 0,
   search: 0,
 };
 
-const SHELL_TRANSPARENCY = "shell-transparency";
+export const SHELL_TRANSPARENCY = "shell-transparency";
 
-function init() {
+export function init() {
   log("Starting transparent-shell extension");
 }
 
-const toggleTransparency = (el, enabled = true) =>
+export const toggleTransparency = (el, enabled = true) =>
   el &&
   el[enabled ? "add_style_class_name" : "remove_style_class_name"](
     SHELL_TRANSPARENCY
   );
 
-const componentToggles = {
+export const componentToggles = {
   "top-panel": (isEnabled) => {
     try {
-      [Main.panel]
-        .concat(Main?.mmPanel)
+      [main.panel]
+        .concat(main?.mmPanel)
         .filter((p) => !!p)
         .forEach((mmpanel) => {
           toggleTransparency(mmpanel, isEnabled);
@@ -38,29 +39,31 @@ const componentToggles = {
     }
   },
   "dash": (isEnabled) =>
-    toggleTransparency(Main.overview.dash._background, isEnabled),
+    toggleTransparency(overview._dash.actor, isEnabled),
   "search": (isEnabled) =>
-    toggleTransparency(Main.overview.searchEntry, isEnabled),
+    toggleTransparency(overview._searchEntry, isEnabled),
 };
 
-const onChange = (_, key) => {
+export const onChange = (_, key) => {
   const isEnabled = this.settings.get_boolean(key);
   if (componentToggles[key]) {
     componentToggles[key](isEnabled);
   }
 };
 
-function enable() {
+export function enable() {
   this.settings = ExtensionUtils.getSettings();
   Object.keys(settingskeys).forEach((key) => {
     if (this.settings.get_boolean(key)) {
       componentToggles[key](true);
     }
-    settingskeys[key] = this.settings.connect("changed::" + key, onChange);
+    settingskeys[key] = this.settings.connect("changed::" + key, () =>
+      onChange(this.settings, key)
+    );
   });
 }
 
-function disable() {
+export function disable() {
   Object.keys(settingskeys).forEach((key) => {
     componentToggles[key](false);
     this.settings.disconnect(settingskeys[key]);
